6a6c5859fb0fc5329d693846e42ef9cac5cf4b72
rajinwonderland <raj@novvuminc.com>
2019-01-22 15:05:24 -0800

    Updated

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,29 +1,23 @@
-import { ApolloServer } from "apollo-server";
-import MarvelApiModel from "./models/MarvelApiModel";
-import { schema } from "./resolvers";
-import CharacterModel from "./models/CharacterModel";
-import ComicModel from "./models/ComicModel";
-import CreatorModel from "./models/CreatorModel";
-import EventModel from "./models/EventModel";
-import SeriesModel from "./models/SeriesModel";
-import StoryModel from "./models/StoryModel";
+import { ApolloServer } from 'apollo-server';
+import { schema } from './resolvers';
+import { Prisma } from './generated/prisma';
+
+const db = new Prisma({
+	endpoint: process.env.PRISMA_ENDPOINT!,
+	secret: process.env.PRISMA_SECRET!,
+	debug: true,
+});
 
 const server = new ApolloServer({
-  schema,
-  context: req => ({
-    ...req,
-    api: new MarvelApiModel(),
-    charactersModel: new CharacterModel(),
-    comicsModel: new ComicModel(),
-    creatorsModel: new CreatorModel(),
-    eventsModel: new EventModel(),
-    seriesModel: new SeriesModel(),
-    storiesModel: new StoryModel()
-  }),
-  engine: {
-    apiKey: "service:marvelQL:31heDXzZ0JWmMz7L4zCuug"
-  }
+	schema,
+	context: req => ({
+		...req,
+		db,
+	}),
+	engine: {
+		apiKey: 'service:marvelQL:31heDXzZ0JWmMz7L4zCuug',
+	},
 });
 server.listen().then(({ url }) => {
-  console.log(`🚀  Server ready at ${url}`);
+	console.log(`🚀  Server ready at ${url}`);
 });
--- server/src/resolvers/Query.ts
+++ server/src/resolvers/Query.ts
@@ -1,5 +1,11 @@
 import { Context } from '../utils/getContext';
 
 export const Query = {
-
+	user: (_, args, ctx: Context, info) => {
+		return ctx.db.query.user({
+			where: {
+				...args,
+			},
+		});
+	},
 };
